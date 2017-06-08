ce4d1b36efa17c7226f7e02998eb6eada3378d88
Giousa <65489469@qq.com>
2017-06-08 13:23:34 +0800

    微调

--- module_setting/src/main/java/com/zmm/module_setting/act/TwSettingActivity.java
+++ module_setting/src/main/java/com/zmm/module_setting/act/TwSettingActivity.java
@@ -14,7 +14,7 @@ import com.zmm.module_setting.base.TwBaseActivity;
  * Time:上午10:42
  */
 
-public class TwSettingActivity extends TwBaseActivity implements View.OnClickListener {
+public class TwSettingActivity extends TwBaseActivity{
 
     private final String TAG = TwSettingActivity.class.getSimpleName();
     private RelativeLayout mUserEdit,mResetPassword,mReportTitle,mAbout,mLogout;
@@ -33,11 +33,42 @@ public class TwSettingActivity extends TwBaseActivity implements View.OnClickLis
         mAbout = (RelativeLayout) findViewById(R.id.rl_about);
         mLogout = (RelativeLayout) findViewById(R.id.rl_logout);
 
-        mUserEdit.setOnClickListener(this);
-        mResetPassword.setOnClickListener(this);
-        mReportTitle.setOnClickListener(this);
-        mAbout.setOnClickListener(this);
-        mLogout.setOnClickListener(this);
+//        mUserEdit.setOnClickListener(this);
+//        mResetPassword.setOnClickListener(this);
+//        mReportTitle.setOnClickListener(this);
+//        mAbout.setOnClickListener(this);
+//        mLogout.setOnClickListener(this);
+
+        mUserEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TwUserEditActivity.class,false);
+            }
+        });
+        mResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TwResetPasswordActivity.class,false);
+            }
+        });
+        mReportTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TwLogoActivity.class,false);
+            }
+        });
+        mAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TwAboutActivity.class,false);
+            }
+        });
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(TwLogOutActivity.class,false);
+            }
+        });
     }
 
     @Override
@@ -45,38 +76,38 @@ public class TwSettingActivity extends TwBaseActivity implements View.OnClickLis
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.rl_user_edit:
-                Log.d(TAG,"rl_user_edit");
-                startActivity(TwUserEditActivity.class,false);
-                break;
-
-            case R.id.rl_reset_password:
-                Log.d(TAG,"rl_reset_password");
-                startActivity(TwResetPasswordActivity.class,false);
-
-                break;
-
-            case R.id.rl_report_title:
-                Log.d(TAG,"rl_report_title");
-                startActivity(TwLogoActivity.class,false);
-
-                break;
-
-            case R.id.rl_about:
-                Log.d(TAG,"rl_about");
-                startActivity(TwAboutActivity.class,false);
-
-                break;
-
-            case R.id.rl_logout:
-                Log.d(TAG,"rl_logout");
-                startActivity(TwLogOutActivity.class,false);
-
-                break;
-
-        }
-    }
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.rl_user_edit:
+//                Log.d(TAG,"rl_user_edit");
+//                startActivity(TwUserEditActivity.class,false);
+//                break;
+//
+//            case R.id.rl_reset_password:
+//                Log.d(TAG,"rl_reset_password");
+//                startActivity(TwResetPasswordActivity.class,false);
+//
+//                break;
+//
+//            case R.id.rl_report_title:
+//                Log.d(TAG,"rl_report_title");
+//                startActivity(TwLogoActivity.class,false);
+//
+//                break;
+//
+//            case R.id.rl_about:
+//                Log.d(TAG,"rl_about");
+//                startActivity(TwAboutActivity.class,false);
+//
+//                break;
+//
+//            case R.id.rl_logout:
+//                Log.d(TAG,"rl_logout");
+//                startActivity(TwLogOutActivity.class,false);
+//
+//                break;
+//
+//        }
+//    }
 }
